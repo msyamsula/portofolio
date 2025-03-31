7baec8d6f994bbd917b0c6ce742efb3bcf19c3e5
syamsul.arifin <syamsul.arifin@bytedance.com>
2025-03-31 23:48:51 +0700

    fix: empty event listener

--- ui/chat/index.js
+++ ui/chat/index.js
@@ -8,24 +8,24 @@ async function login(username) {
 
 }
 
-document.getElementById("login-form").addEventListener("submit", async function (event) {
-    event.preventDefault();
-    const username = document.getElementById("username").value;
+// document.getElementById("login-form").addEventListener("submit", async function (event) {
+//     event.preventDefault();
+//     const username = document.getElementById("username").value;
 
-    user = await login(username)
-    if (user) {
-        localStorage.setItem("username", user.username);
-        localStorage.setItem("id", user.id);
-        let msg = {
-            userId: getUserId()
-        }
-        socket.emit("userLogin", msg)
-        window.location.href = "chat.html"; // Redirect to chat page
-        return
-    }
+//     user = await login(username)
+//     if (user) {
+//         localStorage.setItem("username", user.username);
+//         localStorage.setItem("id", user.id);
+//         let msg = {
+//             userId: getUserId()
+//         }
+//         socket.emit("userLogin", msg)
+//         window.location.href = "chat.html"; // Redirect to chat page
+//         return
+//     }
 
-    alert("try different name")
-});
+//     alert("try different name")
+// });
 
 window.onload = function () {
     const username = localStorage.getItem("username");
