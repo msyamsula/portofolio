a5efb54229c3ed60188c704169b3da4299be1dd0
msyamsula <msyamsula1995@gmail.com>
2025-11-19 12:28:36 +0700

    use telemetryv2

--- backend-app/url-shortener/main.go
+++ backend-app/url-shortener/main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/msyamsula/portofolio/backend-app/url-shortener/handler"
 	"github.com/msyamsula/portofolio/backend-app/url-shortener/persistent"
 	"github.com/msyamsula/portofolio/backend-app/url-shortener/services"
-	"github.com/msyamsula/portofolio/binary/telemetry"
+	"github.com/msyamsula/portofolio/telemetryv2"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/cors"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
@@ -44,7 +44,7 @@ var (
 func Route(r *mux.Router) *mux.Router {
 
 	// initialize instrumentation
-	telemetry.InitializeTelemetryTracing(appName, jaegerHost)
+	telemetryv2.InitializeTelemetryTracing(appName, jaegerHost)
 
 	h := handler.New(handler.Config{
 		Svc: services.New(services.Config{
