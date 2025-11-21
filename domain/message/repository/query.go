a5b87395a62a225c3e3b9bd1241f6579de1a9d07
msyamsula <msyamsula1995@gmail.com>
2025-11-22 06:47:35 +0700

    refactor

--- domain/message/repository/query.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-var (
-	QueryInsertMessage   = "INSERT INTO messages (sender_id, receiver_id, text) VALUES (:sender_id, :receiver_id, :text) RETURNING id"
-	QueryGetConversation = "SELECT id, sender_id, receiver_id, text, create_time, is_read FROM messages m WHERE sender_id = :sender_id AND receiver_id = :receiver_id"
-	QueryReadMessage     = "UPDATE messages SET is_read = true WHERE sender_id = :sender_id AND receiver_id = :receiver_id"
-	QueryUpdateUnread    = "insert into unread (sender_id, receiver_id, unread) values (:sender_id, :receiver_id, :unread) on conflict (sender_id, receiver_id) do update set unread = excluded.unread returning id;"
-)
--- domain/message/service/error.go
+++
@@ -1,7 +0,0 @@
-package service
-
-import "errors"
-
-var (
-	ErrBadRequest = errors.New("bad request in message body")
-)
