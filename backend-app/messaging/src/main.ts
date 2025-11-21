4cfd9f4e2f5542cbbdace8828ff72f019b2d2531
msyamsula <msyamsula1995@gmail.com>
2025-11-21 17:29:00 +0700

    main

--- backend-app/messaging/src/main.ts
+++ backend-app/messaging/src/main.ts
@@ -1,22 +1,55 @@
 import { websocket } from "./websocket/websocket.js"
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
+import { newSqsSnsAdapter } from "./adapter/sqs-sns.js";
+import { newMongoAdapter, getMonggoCollectionAdapter } from "./adapter/mongo.js";
 
+const ENVIRONMENT = process.env.ENVIRONMENT
 const PORT = process.env.PORT || "3000";
+const MONGO_ADAPTER_DB = process.env.MONGO_DB || "my-mongodb"
+const MONGO_ADAPTER_COLLECTION = process.env.MONGO_COLLECTION || "socket-io-adapter"
+const MONGO_ADAPTER_URI = process.env.MONGO_URI || "mongodb://localhost:27017"
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || ""
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || ""
+const AWS_REGION = process.env.AWS_REGION || ""
 
-function main() {
-    const app = express();
-    const server = http.createServer(app);
+function showEnv() {
+    console.log(ENVIRONMENT);
+    console.log(PORT);
+    console.log(MONGO_ADAPTER_DB)
+    console.log(MONGO_ADAPTER_COLLECTION)
+    console.log(MONGO_ADAPTER_URI)
+    console.log(AWS_SECRET_ACCESS_KEY);
+    console.log(AWS_ACCESS_KEY_ID);
+    console.log(AWS_REGION);
+}
 
-    const io = new Server(server, {
+
+async function main() {
+    if (ENVIRONMENT != "production") {
+        showEnv()
+    }
+
+
+    let adapter = newSqsSnsAdapter({
+        topicName: "socket-io"
+    });
+
+    // const mongoCollectionAdapter = await getMonggoCollectionAdapter(MONGO_ADAPTER_DB, MONGO_ADAPTER_COLLECTION, MONGO_ADAPTER_URI);
+    // let adapter = newMongoAdapter(mongoCollectionAdapter);
+
+
+    let ws = new websocket(PORT, {
         cors: {
             origin: "*",
         },
+        connectionStateRecovery: {
+            maxDisconnectionDuration: 2 * 60 * 1000,
+            skipMiddlewares: true
+        },
+        transports: ["websocket"],
+        // adapter: adapter,
     });
 
-    let ws = new websocket(io, server, PORT);
     ws.run();
 }
 
-main();
+await main();
