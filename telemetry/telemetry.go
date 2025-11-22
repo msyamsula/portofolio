2a5a22b5c50b404234fbb94eeb83fe7d8001911f
msyamsula <msyamsula1995@gmail.com>
2025-11-23 02:32:03 +0700

    update tracing package

--- telemetry/telemetry.go
+++ telemetry/telemetry.go
@@ -5,55 +5,32 @@ import (
 	"log"
 
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
-	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 )
 
-func InitializeTelemetryTracing(appName, jaegerHost string) {
+func InitializeTelemetryTracing(appName, jaegerHost string) func() {
 	// Initialize OpenTelemetry SDK
 	ctx := context.Background()
 	exporterOptions := []otlptracegrpc.Option{
 		otlptracegrpc.WithEndpoint(jaegerHost),
-		otlptracegrpc.WithInsecure(), // remove this for production
 	}
 	exporter, err := otlptracegrpc.New(ctx, exporterOptions...)
 	if err != nil {
 		log.Fatal("failed to create exporter")
 	}
 
-	attr := []attribute.KeyValue{
-		{
-			Key:   attribute.Key("opentelemetry.io/schemas"),
-			Value: attribute.StringValue("1.7.0"),
-		},
-		{
-			Key:   attribute.Key("service.name"),
-			Value: attribute.StringValue(appName),
-		},
-	}
-
-	// resource
-	resource, err := resource.New(ctx,
-		resource.WithAttributes(
-			attr...,
-		),
-		resource.WithSchemaURL(semconv.SchemaURL),
-	)
-	if err != nil {
-		log.Fatal("failed to create resource")
-	}
-
 	// tracer provider
 	tracerProvider := sdktrace.NewTracerProvider(
 		sdktrace.WithBatcher(exporter),
-		sdktrace.WithResource(resource),
+		sdktrace.WithResource(resource.Default()),
 	)
 
 	otel.SetTracerProvider(tracerProvider)
-	otel.SetTextMapPropagator(propagation.TraceContext{})
+
+	return func() {
+		tracerProvider.Shutdown(ctx)
+	}
 
 }
