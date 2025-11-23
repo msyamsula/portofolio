028c819f661637e6f120fe54c91891fc6623de17
msyamsula <msyamsula1995@gmail.com>
2025-11-23 23:32:59 +0700

    update telemetry

--- telemetry/telemetry.go
+++ telemetry/telemetry.go
@@ -7,6 +7,7 @@ import (
 	"go.opentelemetry.io/otel/exporters/zipkin"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
 
 func InitializeTelemetryTracing(appName, tracerCollectorEndpoint string) func() {
@@ -17,11 +18,23 @@ func InitializeTelemetryTracing(appName, tracerCollectorEndpoint string) func() 
 		return func() {}
 	}
 
+	res, err := resource.New(
+		context.Background(),
+		resource.WithAttributes(
+			semconv.ServiceName(appName),
+			semconv.ServiceVersion("v1.0.0"),
+			semconv.DeploymentEnvironment("local"),
+		),
+	)
+	if err != nil {
+		panic(err)
+	}
+
 	// tracer provider
 	tracerProvider := sdktrace.NewTracerProvider(
 		// sdktrace.WithBatcher(exporter), // batch send to collector
 		sdktrace.WithSyncer(exporter), // always send to colletor, good for debugging not for production
-		sdktrace.WithResource(resource.Default()),
+		sdktrace.WithResource(res),
 		sdktrace.WithSampler(sdktrace.AlwaysSample()), // again, not for production, use sampling deliberately
 		// sdktrace.WithSampler(sdktrace.TraceIDRatioBased(0.1)), // this is good choice for start, sampling 10% traffic in local sampler
 	)
