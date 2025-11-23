832e58ee998730c87cdb464e06808112e0689da4
msyamsula <msyamsula1995@gmail.com>
2025-11-23 10:23:36 +0700

    use zipkin exporter

--- telemetry/telemetry.go
+++ telemetry/telemetry.go
@@ -2,29 +2,28 @@ package telemetry
 
 import (
 	"context"
-	"log"
 
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/exporters/zipkin"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
-func InitializeTelemetryTracing(appName, jaegerHost string) func() {
+func InitializeTelemetryTracing(appName, tracerCollectorEndpoint string) func() {
 	// Initialize OpenTelemetry SDK
 	ctx := context.Background()
-	exporterOptions := []otlptracegrpc.Option{
-		otlptracegrpc.WithEndpoint(jaegerHost),
-	}
-	exporter, err := otlptracegrpc.New(ctx, exporterOptions...)
+	exporter, err := zipkin.New(tracerCollectorEndpoint)
 	if err != nil {
-		log.Fatal("failed to create exporter")
+		return func() {}
 	}
 
 	// tracer provider
 	tracerProvider := sdktrace.NewTracerProvider(
-		sdktrace.WithBatcher(exporter),
+		// sdktrace.WithBatcher(exporter), // batch send to collector
+		sdktrace.WithSyncer(exporter), // always send to colletor, good for debugging not for production
 		sdktrace.WithResource(resource.Default()),
+		sdktrace.WithSampler(sdktrace.AlwaysSample()), // again, not for production, use sampling deliberately
+		// sdktrace.WithSampler(sdktrace.TraceIDRatioBased(0.1)), // this is good choice for start, sampling 10% traffic in local sampler
 	)
 
 	otel.SetTracerProvider(tracerProvider)
